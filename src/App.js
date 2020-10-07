f5c2f2ccad332dc2508d48b8ba6b00027f6dd7b8
bbsquirrel247 <aaronjosephcunningham@gmail.com>
2020-10-07 17:07:14 +0200

    ship

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { Canvas, extend, useFrame, useThree } from 'react-three-fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Canvas, extend, useThree, useFrame } from 'react-three-fiber';
 import { useSpring, a } from 'react-spring/three';
 
 extend({ OrbitControls });
 
+const SpaceShip = () => {
+  const [model, setModel] = useState();
+
+  useEffect(() => {
+    new GLTFLoader().load('/scene.gltf', setModel);
+  });
+
+  return model ? <primitive object={model.scene} /> : null;
+};
+
 const Controls = () => {
   const orbitRef = useRef();
   const { camera, gl } = useThree();
-  return <orbitControls args={[camera, gl.domElement]} ref={orbitRef} />;
+
+  useFrame(() => {
+    orbitRef.current.update();
+  });
+  return (
+    <orbitControls
+      autoRotate
+      // enableDamping
+      // maxPolarAngle={Math.PI / 3}
+      // minPolarAnlge={Math.PI / 3}
+      args={[camera, gl.domElement]}
+      ref={orbitRef}
+    />
+  );
 };
+
+// const Plane = () => (
+//   <mesh rotation={[-Math.PI / 2, 0, 0]}>
+//     <planeBufferGeometry attach="geometry" args={[100, 100]} />
+//     <meshPhysicalMaterial attach="material" color="white" />
+//   </mesh>
+// );
 
 const Box = () => {
   const [hovered, setHovered] = useState(false);
   const [active, setActive] = useState(false);
   const props = useSpring({
-    scale: active ? [2, 2, 2] : [3, 3, 3],
+    scale: active ? [1, 1, 1] : [1, 1, 1],
     color: hovered ? 'pink' : 'lightblue',
   });
-  // useFrame(() => {
-
-  // });
 
   return (
     <a.mesh
@@ -29,16 +58,21 @@ const Box = () => {
       onClick={() => setActive(!active)}
       scale={props.scale}
     >
+      <ambientLight />
+      <spotLight position={[0, 5, 10]} penumbra="1" />
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <a.meshBasicMaterial attach="material" color={props.color} />
+      <a.meshPhysicalMaterial attach="material" color={props.color} />
     </a.mesh>
   );
 };
 
 const App = () => {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [0, 0, 15] }}>
+      {/* <fog attach="fog" args={['white', 10, 20]} /> */}
+      <Controls />
       <Box />
+      <SpaceShip />
     </Canvas>
   );
 };
